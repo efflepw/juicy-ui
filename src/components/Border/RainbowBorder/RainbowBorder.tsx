ee624e36ac17ab079dc320789b3bb880733787d9
Yaroslav Sievastianov <yaroslav.sievastianov@gmail.com>
2024-07-24 20:14:23 -0700

    feat: destroy init

--- src/components/Border/RainbowBorder/RainbowBorder.tsx
+++ src/components/Border/RainbowBorder/RainbowBorder.tsx
@@ -1,4 +1,4 @@
-import "./styles.css";
+import "./RainbowBorder.css";
 
 type Props = {
   children: React.ReactNode;
---
+++ src/components/Destroy/MonoParticles/MonoParticles.tsx
@@ -0,0 +1,12 @@
+import "./MonoParticles.css";
+
+const MonoParticles = () => {
+  return (
+    <div className="mono-particles-container">
+      <canvas className="mono-particles-canvas"></canvas>
+      <div className="mono-particles-content"></div>
+    </div>
+  );
+};
+
+export default MonoParticles;
