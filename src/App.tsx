e138bf75eed5c672707521ea5c852719ba0e1c10
Yaroslav Sievastianov <yaroslav.sievastianov@gmail.com>
2024-07-31 00:19:28 -0700

    feat: components preview (#5)
    * feat: add tailwind for components preview
    add components preview page structure
    
    * feat: continue working on structure and design
    add canvas on bg
    
    * feat: add all current component to preview
    
    * feat: small improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,85 @@
+import { useState } from "react";
 import "./App.css";
+import Navbar from "./components.preview/Navbar";
+import LikeButtons from "./components.preview/LikeButtons";
+import { Ambient } from "./components/Image";
+import { MonoParticles } from "./components/Destroy";
+import { RainbowBorder } from "./components/Border";
 import { Particles } from "./components/Background";
 
+type Section = {
+  id: string;
+  name: string;
+  component: React.ReactElement;
+};
+
+const SECTION_COMPONENTS: Section[] = [
+  {
+    id: "mono-particles",
+    name: "Mono Particles",
+    component: <MonoParticles />,
+  },
+  {
+    id: "rainbow-border",
+    name: "Rainbow border",
+    component: (
+      <RainbowBorder>
+        <div
+          style={{ backgroundColor: "#000000" }}
+          className="test-block"
+        ></div>
+      </RainbowBorder>
+    ),
+  },
+  {
+    id: "ambient",
+    name: "Ambient",
+    component: <Ambient />,
+  },
+  {
+    id: "like-button",
+    name: "Like button",
+    component: <LikeButtons />,
+  },
+  {
+    id: "particles-background",
+    name: "Particles background",
+    component: <Particles />,
+  },
+];
+
 const App = () => {
+  const [selectedSection, setSelectedSection] = useState<Section>(
+    SECTION_COMPONENTS[0]
+  );
+
+  const onSelectSection = (sectionId: string) => {
+    const newSection = SECTION_COMPONENTS.find(({ id }) => id == sectionId);
+
+    if (newSection) {
+      setSelectedSection(newSection);
+    }
+  };
+
   return (
-    <main>
-      <div className="centered-div">
-        <Particles />
+    <>
+      <div className="h-screen flex flex-col p-2 gap-2">
+        <header className="h-24 bg-secondary rounded-3xl"></header>
+        <main
+          className="grid flex-grow gap-1"
+          style={{ gridTemplateColumns: "1fr 5fr" }}
+        >
+          <Navbar
+            activeId={selectedSection.id}
+            sections={SECTION_COMPONENTS}
+            setSelectedSection={onSelectSection}
+          />
+          <div className="flex items-center justify-center rounded-3xl bg-lightdark">
+            {selectedSection.component}
+          </div>
+        </main>
       </div>
-    </main>
+    </>
   );
 };
 
---
+++ src/components.preview/Navbar/Navbar.tsx
@@ -0,0 +1,35 @@
+type NavSection = {
+  id: string;
+  name: string;
+};
+
+type Props = {
+  activeId: string;
+  sections: NavSection[];
+  setSelectedSection: (id: string) => void;
+};
+
+const Navbar = ({ activeId, sections, setSelectedSection }: Props) => {
+  return (
+    <nav className="flex-grow  flex flex-col items-center">
+      <div className="bg-lightdark rounded-3xl py-6 px-8 h-full">
+        {sections.map(({ id, name }) => (
+          <div
+            className="w-52 cursor-pointer hover:underline text-lg leading-relaxed"
+            onClick={() => setSelectedSection(id)}
+          >
+            <span
+              className={`${
+                activeId == id ? "text-white underline" : "text-gray"
+              }`}
+            >
+              {name}
+            </span>
+          </div>
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
