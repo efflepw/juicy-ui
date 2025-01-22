f6a2ccd501c4b5689d829391ad916cfaf6929561
Yaroslav Sievastianov <yaroslav.sievastianov@gmail.com>
2025-01-21 23:29:03 -0800

    feat: change a to link

--- src/components.preview/Header/Header.tsx
+++ src/components.preview/Header/Header.tsx
@@ -1,19 +1,21 @@
+import { Link } from "react-router";
+
 const NAV_LINKS = [
   { name: "Components", href: "/" },
   // { name: "Classes", href: "classes" },
   // { name: "Components", href: "components" },
-  { name: "Palettes", href: "palettes" },
+  { name: "Palettes", href: "/palettes" },
   // { name: "Contribute", href: "contribute" },
 ];
 
 const Header = () => {
   return (
     <header className="bg-secondary rounded-lg py-3 px-6 text-xl fixed top-3 w-[80%] backdrop-blur-sm left-1/2 transform -translate-x-1/2 z-50 flex">
-      <nav className="w-full flex gap-4 justify-around">
+      <nav className="w-full flex gap-4 justify-around text-primary">
         {NAV_LINKS.map(({ name, href }) => (
-          <a key={name} href={href} className="text-primary">
+          <Link key={name} to={href} className="text-primary">
             {name}
-          </a>
+          </Link>
         ))}
       </nav>
     </header>
--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/juicy-ui">
+    <BrowserRouter basename="/juicy-ui/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
