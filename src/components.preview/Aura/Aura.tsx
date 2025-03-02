addd8fbea1f7a83937abc18ed2b6cd36e3cf7c75
Yaroslav Sievastianov <yaroslav.sievastianov@gmail.com>
2025-03-01 22:43:54 -0800

    feat: add palette selector for Aura effect

--- src/components.preview/Aura/Aura.tsx
+++ src/components.preview/Aura/Aura.tsx
@@ -1,19 +1,38 @@
+import { useState } from "react";
 import PageEnd from "../../components/Aura/PageEnd";
 import TextGradient from "../../components/Text/TextGradient";
 import { BASE_COLORS } from "../../const/colors";
 import { Palette } from "../../utils/palette";
+import PaletteSelect from "../PaletteSelect";
+import { BASE_PALETTES } from "../../const/palette";
 
 const AuraPreview = () => {
-  const palette1 = new Palette(BASE_COLORS["50_SHADES"], 45);
-  const palette2 = new Palette(BASE_COLORS.TAILWIND, 45);
+  const textPalette = new Palette(BASE_COLORS["TAILWIND"], 45);
+  const defaultPalette = BASE_PALETTES[0];
+  const [selectedPalette, setSelectedPalette] =
+    useState<Palette>(defaultPalette);
 
   return (
-    <div className="h-[1000px]">
-      <TextGradient
-        text={"Scroll to the bottom of the page"}
-        gradient={palette1.getLinearGradient()}
-      />
-      <PageEnd palette={palette2} />
+    <div className="h-[1000px] w-full">
+      <div className="py-6 flex items-center justify-center gap-24">
+        <TextGradient
+          className="text-2xl"
+          text={"Select Aura effect palette"}
+          gradient={textPalette.getLinearGradient()}
+        />
+        <PaletteSelect
+          defaultPalette={defaultPalette}
+          palettes={BASE_PALETTES}
+          onChange={(p: Palette) => setSelectedPalette(p)}
+        />
+      </div>
+      <div className="w-full flex justify-center pt-10">
+        <TextGradient
+          text={"Scroll to the bottom of the page"}
+          gradient={textPalette.getLinearGradient()}
+        />
+      </div>
+      <PageEnd palette={selectedPalette} />
     </div>
   );
 };
---
+++ src/components.preview/PaletteSelect/PaletteSelect.tsx
@@ -0,0 +1,102 @@
+import { useState, useRef, useEffect } from "react";
+import { Palette } from "../../utils/palette";
+
+type GradientSelectProps = {
+  defaultPalette: Palette | null;
+  palettes: Palette[];
+  onChange?: (item: Palette) => void;
+  placeholder?: string;
+};
+
+// @todo add appearance one by one
+const GradientSelect = ({
+  defaultPalette,
+  palettes,
+  onChange,
+  placeholder = "Select palette",
+}: GradientSelectProps): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<Palette | null>(
+    defaultPalette
+  );
+  const selectRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        selectRef.current &&
+        !selectRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleSelect = (item: Palette) => {
+    setSelectedItem(item);
+    setIsOpen(false);
+    if (onChange) {
+      onChange(item);
+    }
+  };
+
+  return (
+    <div ref={selectRef} className="relative w-56">
+      <div
+        className="flex items-center justify-between border border-gray-300 rounded-md cursor-pointer hover:bg-secondary"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {selectedItem ? (
+          <div className="flex items-center justify-between cursor-pointer p-1 hover:bg-secondary w-full rounded-md">
+            <div
+              className="w-full h-6 rounded-sm"
+              style={{ background: selectedItem.getLinearGradient() }}
+            ></div>
+          </div>
+        ) : (
+          <span className="text-gray-400 p-2">{placeholder}</span>
+        )}
+        <svg
+          className={`w-4 h-4 transition-transform duration-200 ml-1 mr-2 ${
+            isOpen ? "transform rotate-180" : ""
+          }`}
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M19 9l-7 7-7-7"
+          />
+        </svg>
+      </div>
+
+      {isOpen && (
+        <div className="absolute z-10 mt-1 border border-gray-300 rounded-md shadow-lg max-h-60 overflow-auto w-full">
+          {palettes.map((item) => (
+            <div
+              key={item.getName()}
+              className="flex items-center justify-between cursor-pointer px-1 pb-1 hover:bg-secondary w-full rounded-md first:pt-1"
+            >
+              <div
+                className="w-full h-6 rounded-sm"
+                style={{ background: item.getLinearGradient() }}
+                onClick={() => handleSelect(item)}
+              ></div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default GradientSelect;
